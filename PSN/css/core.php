dcbb869d31375b7549d61a96adad1a15e54d3311
Griffin Wiles <griffin.wiles1@gmail.com>
2018-10-17 19:25:28 -0400

    Worked on responsive design for header and index

--- PSN/css/core.php
+++ PSN/css/core.php
@@ -12,7 +12,7 @@
                   'buttonhover'=>'#8405af',
                   'buttontext'=>'#f3e1ff',
                   'commentbox'=>'#fbfbfb'],
-        'Dark' =>['background'=>'#140026', 
+        'Dark' =>['background'=>'#140026',
                   'text'=>'rgba(255,255,255,.65)',
                   'boxcolor'=>'#1b0036',
                   'link'=>'rgba(200,0,200,.65)',
@@ -100,7 +100,7 @@
         height:35px;
         border-color:rgb(0,0,0,.15);
         border-radius:5px;
-        text-indent: 5px; 
+        text-indent: 5px;
     }
 
     textarea {
@@ -219,13 +219,13 @@
     ::-webkit-scrollbar {
         width: 15px;
     }
-    
+
     ::-webkit-scrollbar-track {
         transform:translateX(-5px);
     }
-    
+
     ::-webkit-scrollbar-thumb {
-        background: #cccccc; 
+        background: #cccccc;
         border-radius: 8px;
     }
 
@@ -244,7 +244,7 @@
         position:fixed;
         z-index:50;
     }
-    
+
     .header-box{
         width:93%;
         margin:auto;
@@ -255,7 +255,7 @@
     .header-link-box{
         width:40%;
     }
-    
+
     .header-link>a{
         color:<?php echo $textcolor ?>;
         text-decoration:none;
@@ -266,7 +266,7 @@
         width: calc(100% + 40px);
         transform: translateX(-20px);
     }
-    
+
     .header-link{
         height:100%;
         width:auto;
@@ -276,23 +276,23 @@
         padding-right:20px;
         padding-left:20px;
     }
-    
+
     .header-link:hover{
         height:calc(100% - 2px);
         border-bottom-width:3px;
         border-bottom-color: <?php echo $linkcolor ?>;
     }
-    
+
     .active{
         height:calc(100% - 2px);
         border-bottom-width:3px;
         border-bottom-color: <?php echo $linkcolor?>;
     }
-    
+
     .active a{
         color:<?php echo $linkcolor ?>;
     }
-    
+
     .header-link:hover a{
         color:<?php echo $linkcolor?>;
     }
@@ -350,11 +350,11 @@
         border-bottom-width:1px;
         border-bottom-style:solid;
     }
-    
+
     .header-profile-menu-name:hover{
         background-color:<?php echo $backgroundcolor ?>;
     }
-    
+
     .header-profile-menu-name-name>a{
         color:<?php echo $boldcolor ?>;
         font-weight:bold;
@@ -366,15 +366,15 @@
         padding-left:10px;
         width:calc(100% - 10px);
     }
-    
+
     .header-profile-menu-name-name{
         height:40%;
     }
-    
+
     .header-profile-menu-name-username{
         height:60%;
     }
-    
+
     .header-profile-menu-name-username>a{
         padding-bottom:10px;
         color:<?php echo $textcolor ?>;
@@ -503,7 +503,7 @@
         margin:auto;
         height:auto;
         padding-bottom:25px;
-        padding-top:60px;  
+        padding-top:60px;
     }
 
     .index-left-box{
@@ -533,7 +533,7 @@
       position: relative;
       height:75px;
     }
-    
+
     .compose-prayer iframe {
        border: 0;
        height: 100%;
@@ -705,7 +705,7 @@
         color: #fff;
         transform: translateY(-100px);
     }
-/*************** USERSETTINGS ************/ 
+/*************** USERSETTINGS ************/
     .usersettings-box{
         display:block;
         margin-block-start: 0em;
@@ -736,7 +736,7 @@
     .settings-link:hover{
         background-color:<?php echo $linkcolor ?>;
     }
-    
+
     .current{
         background-color:<?php echo $linkcolor ?>;
     }
@@ -766,7 +766,7 @@
         width:100%;
         height:auto;
     }
-    
+
     .feed-box{
         padding:30px 20px 30px 20px;
         display:flex;
@@ -774,11 +774,11 @@
         border-bottom-color:<?php echo $backgroundcolor ?>;
         border-bottom-width:1px;
     }
-    
+
     .feed-profile-img-box{
         width:10%;
     }
-    
+
     .feed-profile-img{
         height:46px;
         width:46px;
@@ -840,17 +840,17 @@
         margin-bottom:5px;
         vertical-align: middle;
     }
-    
+
     .feed-img{
         margin:auto;
         width:100%;
         position:relative;
     }
-    
+
     .feed-img:hover{
         cursor:pointer;
     }
-    
+
     .feed-img-container{
         max-height:400px;
         margin:auto;
@@ -860,17 +860,17 @@
         border-width:1px;
         border-radius:5px;
     }
-    
+
     .imglarge-box{
         width:40%;
         margin:auto;
         margin-top:100px;
     }
-    
+
     #imglarge{
         width:100%;
     }
-    
+
     #imglarge-body{
         position:absolute;
         height:100%;
@@ -886,7 +886,7 @@
         padding-bottom:20px;
         background-color:<?php echo $commentcolor ?>;
     }
-    
+
     .post-comment{
         padding-top:20px;
         padding-bottom:20px
@@ -900,12 +900,12 @@
         border-bottom-color:<?php echo $backgroundcolor ?>;
         border-bottom-width:1px;
     }
-    
+
     .comment{
         width:100%;
         margin-bottom:10px;
     }
-    
+
     #submit-comment{
         height:auto;
         background-color:<?php echo $buttoncolor ?>;
@@ -919,14 +919,14 @@
         padding-right:15px;
         padding-left:15px;
         font-weight:bold;
-        
+
     }
-    
+
     #submit-comment:hover{
         cursor:pointer;
         background-color:<?php echo $buttonhover ?>;
     }
-    
+
     .comment-profile-name{
         color:<?php echo $boldcolor?>;
         font-weight:bold;
@@ -1090,7 +1090,7 @@
         margin:auto;
         display:table;
     }
-    
+
     .msg-convo{
         overflow-y:auto;
         display:grid;
@@ -1105,7 +1105,7 @@
         height:auto;
         padding:10px;
     }
-    
+
     .compose-message>form{
         display:flex;
     }
@@ -1154,7 +1154,7 @@
         margin-bottom:10px;
         padding:10px;
         color:#ffffff;
-        float:right;    
+        float:right;
     }
 
     .msg-to-me{
@@ -1201,7 +1201,7 @@
         margin:auto;
         padding-top:25px;
     }
-    
+
     .query-input{
         width:90%;
     }
@@ -1252,9 +1252,133 @@
         white-space:nowrap;
     }
 
-/*************** MEDIA QUERIES ***********/
-    @media screen and (max-width:600px){
-        .header{
-            display:none;
-        }
+/*************** MOBILE ******************/
+@media screen and (max-width: 960px) {
+    /* Move the logo to the right so it doesn't clip into Nav Bar */
+    .logo-li {
+        margin-left: 120px;
     }
+}
+
+@media screen and (max-width: 768px) {
+    /*** INDEX ***/
+    .index-body {
+        width: 96%;
+        padding-top: 120px;
+    }
+
+    .index-left-box {
+        display: none;
+    }
+
+    .index-center-box {
+        width: 98%;
+        margin: 12px 12px 0px 12px;
+    }
+
+    .index-right-box {
+        display: none;
+    }
+
+    /*** HEADER **/
+    /* Hide old menu */
+    .header {
+        display: none;
+    }
+
+    .mobile-header {
+        display: block;
+        height: 60px;
+        width: 100%;
+        border-bottom: 2px solid <?php echo $menuborder?>;
+        background-color: <?php echo $boxcolor ?>;
+        position: fixed;
+        z-index: 50;
+    }
+
+    .mobile-header-link-box {
+        margin: 0px;
+    }
+
+    /* Profile Picture - Left */
+    .mobile-profile {
+        height: 50px;
+        width: 50px;
+        float: right;
+        position: absolute;
+        margin-top: 5px;
+        border-radius: 50%;
+        left: 15%;
+    }
+
+    /* Logo - Center */
+    .mobile-logo {
+        height: 50px;
+        width: 50px;
+        position: absolute;
+        margin-top: 5px;
+        left: 47.2%;
+        border-radius: 50%;
+    }
+
+    /* Notifications - Right */
+    .mobile-notifications {
+        height: 50px;
+        width: 50px;
+        float: right;
+        position: absolute;
+        margin-top: 5px;
+        border-radius: 50%;
+        right: 15%;
+    }
+
+    /* Search Bar */
+    .mobile-search-box {
+        height: 60px;
+        width: 100%;
+        position: absolute;
+        top: 62px;
+        background-color: <?php echo $boxcolor ?>;
+        opacity: 1;
+    }
+
+    .mobile-search-link-box {
+        margin: 0px;
+        height: 60px;
+        margin-bottom: 10px;
+    }
+
+    /* Messages - Left */
+    .mobile-messages {
+        height: 50px;
+        width: 50px;
+        left: 25%;
+        float: right;
+        position: absolute;
+        margin-top: 5px;
+        border-radius: 20px;
+    }
+
+    /* Pray - Right */
+    .mobile-header-link-prayer {
+        margin-top: 10px;
+        float: right;
+        position: absolute;
+        right: 25%;
+    }
+
+    #mobile-start-prayer {
+        height: auto;
+        background-color: <?php echo $buttoncolor ?>;
+        color: <?php echo $buttontext?>;
+        padding: 6px;
+        border-radius: 100px;
+        border-style: solid;
+        border-color: <?php echo $textcolor?>;
+        border-width: 1px;
+        font-size: 22px;
+        padding-right: 15px;
+        padding-left: 15px;
+        font-weight: bold;
+    }
+}
