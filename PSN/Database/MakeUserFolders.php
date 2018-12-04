c6766858b16b6dde18844e21dd21ada4ca406920
RyS0315 <Riley.Stadel1@gmail.com>
2018-12-04 18:39:38 -0500

    wrote a script to give all users their default images

--- PSN/Database/MakeUserFolders.php
+++ PSN/Database/MakeUserFolders.php
@@ -18,12 +18,12 @@
                 continue;
             }
             if($new){
-                mkdir(getRoot()."images/Users/".$id['userid'] );
-                mkdir(getRoot()."images/Users/".$id['userid']."/Profile" );
-                mkdir(getRoot()."images/Users/".$id['userid']."/Banner" );
-                mkdir(getRoot()."images/Users/".$id['userid']."/Uploads" );
-                copy(getRoot()."images/icons/defaultProfile.jpg" , getRoot()."images/Users/".$id['userid']."/Profile/".$id['pPicture']);
-                copy(getRoot()."images/icons/defaultBanner.png" , getRoot()."images/Users/".$id['userid']."/Banner/".$id['bPicture']);
+                mkdir(getRoot()."images/Users/".$i['userid'] );
+                mkdir(getRoot()."images/Users/".$i['userid']."/Profile" );
+                mkdir(getRoot()."images/Users/".$i['userid']."/Banner" );
+                mkdir(getRoot()."images/Users/".$i['userid']."/Uploads" );
+                copy(getRoot()."images/icons/defaultProfile.jpg" , getRoot()."images/Users/".$i['userid']."/Profile/".$i['pPicture']);
+                copy(getRoot()."images/icons/defaultBanner.png" , getRoot()."images/Users/".$i['userid']."/Banner/".$i['bPicture']);
             }
         }
     }
