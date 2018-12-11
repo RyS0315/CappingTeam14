e61a824998fdf60e7ba279c648a35d46b19da37e
RyS0315 <Riley.Stadel1@gmail.com>
2018-12-10 23:13:11 -0500

    DONT CHANGE ADMIN USERNAME

--- PSN/php/updateAccountSettings.php
+++ PSN/php/updateAccountSettings.php
@@ -9,10 +9,12 @@ $curUserInfoquery = "SELECT * FROM USERS WHERE userid = $id";
 $curUserInfo = $db->fetchQuery($curUserInfoquery);
 
 $username = isset($_POST['username']) ? cleanVar($_POST['username']) : $curUserInfo[0]['username'];
-echo $username; 
+
+if($userid == 1){
+    $username = "Admin";
+}
 
 $bio = isset($_POST['bio']) ? cleanVar($_POST['bio']) : $curUserInfo[0]['bio'];
-echo $bio;
 
 //upload profile image
 if ($_FILES['profile']['size'] != 0 && $_FILES['profile']['error'] == 0){
@@ -31,7 +33,6 @@ if ($_FILES['banner']['size'] != 0 && $_FILES['banner']['error'] == 0){
 //update user table
 $query = "UPDATE USERS set username = '$username' , bio = '$bio' WHERE userid = $id";
 $result = $db->UpdateQuery($query);
-echo $result;
 
 //return to settings-account.php
 header('location:'.getRoot().'settings-account.php');
