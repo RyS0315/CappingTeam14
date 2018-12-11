cf2939b8d52dc9c827a8e6be216172a88537db57
RyS0315 <Riley.Stadel1@gmail.com>
2018-12-11 12:34:22 -0500

    asdas

--- PSN/Scripts/createUsers.php
+++ PSN/Scripts/createUsers.php
@@ -5,6 +5,13 @@
     $password = generateRandomString();
     $first = generateRandomString(5);
     $last = generateRandomString(6);
+    
+    $numreligionsquery = "SELECT COUNT(relid) as num
+                          FROM Religions";
+    $numreligionsresult = $db->fetchQuery($numreligionsquery); 
+    $numreligions = $numreligionsresult[0]['num'];
+
+    $prel = Rand(1, $numreligions);
 
     function generateRandomString($length = 10) {
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -17,11 +24,33 @@
         return $randomString;
     }
 
-    $insertquery = "INSERT into USERS (fname,lname,username,user_password,pPicture,bPicture)
-                        VALUES('$first', '$last','$username','$password' , 'default.png', 'default.png')";
+    $insertquery = "INSERT into USERS (fname,lname,username,user_password,pPicture,bPicture, primary_religion)
+                        VALUES('$first', '$last','$username','$password' , 'default.png', 'default.png', '$prel')";
     $insertresult = $db->InsertQuery($insertquery);
 
+
+    
+    
     if($insertresult){
+        $religionInsert = "INSERT into User_Religions(userid, relid)
+                        VALUES('$insertresult', '$prel')";
+        $religion = $db->InsertQuery($religionInsert);
+        createDir($insertresult);
+        setDefaultPhoto($insertresult);
         header('Location:createUsers.php');
+    }
+
+
+
+    function createDir($id){
+        mkdir(getRoot()."images/Users/".$id ,0777, true);
+        mkdir(getRoot()."images/Users/".$id."/Profile" ,0777, true);
+        mkdir(getRoot()."images/Users/".$id."/Banner" ,0777, true);
+        mkdir(getRoot()."images/Users/".$id."/Uploads" ,0777, true);
+    }
+
+    function setDefaultPhoto($id){
+        copy(getRoot()."images/icons/defaultProfile.jpg" , getRoot()."images/Users/".$id."/Profile/default.png");
+        copy(getRoot()."images/icons/defaultBanner.png" , getRoot()."images/Users/".$id."/Banner/default.png");
     }
 ?>
